3123cba9c7ef6bee386b5ab2a249da4abbbc70e8
Rodoro <rodoro.game@gmail.com>
2024-03-24 16:17:45 +0300

    Add heart in book information

--- src/app/api/book/route.ts
+++ src/app/api/book/route.ts
@@ -1,15 +1,26 @@
 import Book from "@/models/Book";
+import User from "@/models/User";
 import connect from "@/utils/db";
+import { getToken } from "next-auth/jwt";
 
 export const POST = async (req: any) => {
     try {
         await connect();
         const { id } = await req.json();
-        const item = await Book.findOne({ id: id });
+        let item = await Book.findOne({ id: id });
         if (!item) {
             return new Response("Книга не найдена", { status: 404 });
         }
-        return Response.json({ item });
+        const session = await getToken({ req });
+
+        if (!session) {
+            return Response.json({ item })
+        }
+        const user = await User.findOne({ email: session.email });
+        if(user.tabs.favourites.includes(id)){
+            item._doc.isActive = true;
+        }
+        return Response.json({ item })
     } catch (error) {
         console.error(error);
         return new Response("Ошибка сервера", { status: 500 });
--- src/containers/book/BookScreen.tsx
+++ src/containers/book/BookScreen.tsx
@@ -2,12 +2,14 @@
 import React, { useEffect, useState } from 'react'
 import Map, { MapProps } from './Map'
 import { Book } from '@/types/books.interface';
+import Heart from '@/components/common/Heart';
 
 export interface BookScreenProps extends MapProps { }
 
 const BookScreen: React.FC<BookScreenProps> = ({ params }: any) => {
     const [book, setBook] = useState<Book>();
     const [isLoading, setIsLoading] = useState(true);
+    const [favourite, setFavourite] = useState(false);
 
     useEffect(() => {
         fetch("/api/book", {
@@ -23,12 +25,34 @@ const BookScreen: React.FC<BookScreenProps> = ({ params }: any) => {
             .then((data) => {
                 setBook(data.item);
                 setIsLoading(false);
+                setFavourite(data.item.isActive)
             })
             .catch((error) => {
                 console.error("Ошибка получения книги")
                 setIsLoading(false);
             })
-    }, [params])
+    }, [ params ])
+
+    const favouriteClick = async () => {
+        if (!book) return;
+        try {
+            await fetch("/api/book/favourite", {
+                method: "POST",
+                headers: {
+                    "Content-Type": "application/json",
+                },
+                body: JSON.stringify({
+                    number: book.id,
+                })
+            })
+                .then(response => response.json())
+                .then((data) => {
+                    setFavourite(data.res);
+                })
+        } catch (error) {
+            console.log(error);
+        }
+    };
 
     if (isLoading) {
         return <div className="flex justify-center items-center">Загрузка...</div>;
@@ -42,7 +66,11 @@ const BookScreen: React.FC<BookScreenProps> = ({ params }: any) => {
                 </div>
             ) : (
                 <div className="grid grid-cols-1 md:grid-cols-2" >
-                    <div className="flex flex-col">
+                    <div className="relative flex flex-col">
+                        <Heart
+                            onClick={favouriteClick}
+                            isActive={favourite}
+                            className='absolute top-0 right-0 mt-2 mr-2' />
                         <h2 className='text-center text-2xl mb-6'>{book.name}</h2>
                         <div className='ml-16'>
                             <div>Автор: {book.author}</div>
